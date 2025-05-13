8ad85066ac833bfca7f67f50357f46d977129c12
saurabhpandey33301 <Saurabh@3301>
2025-05-13 16:47:41 +0530

    new

--- C++ DSA/sample.cpp
+++ C++ DSA/sample.cpp
@@ -3,20 +3,27 @@
 using namespace std;
 
 int main(){
-   string s = "";
-   cin>>s;
-   map<char,int> mp;
+//    string s = "";
+//    cin>>s;
+//    map<char,int> mp;
    
-   int l = s.length();
-   for(int i=0;i<l;i++){
-       mp[s[i]]++;
-   }
-   cout<<"our required frequency is:"<<endl;
-   for(auto it:mp){
-       cout<<it.first<<" "<<it.second;
-       cout<<endl;
-   }
-
+//    int l = s.length();
+//    for(int i=0;i<l;i++){
+//        mp[s[i]]++;
+//    }
+//    cout<<"our required frequency is:"<<endl;
+//    for(auto it:mp){
+//        cout<<it.first<<" "<<it.second;
+//        cout<<endl;
+//    }
+     int arr[5] = {1,2,2,8,5};
+     set <int> s;
+     for(auto it:arr){
+         s.insert(it);
+     }
+    for(auto it:s){
+        cout<<it<<" ";
+    }
     return 0;
 
 }
